f525600f216393d782773c04f30f92a1ab94a4c1
Valeria Biaggi <vbiaggi10@gmail.com>
2021-09-20 09:38:29 -0500

    Pie screen config

---
+++ Challenge/app/components/pie/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import balance from './Balance';
+import yourPies from './YourPies';
+import watchlist from './Watchlist';
+
+export const Balance:React.FC = balance;
+export const YourPies:React.FC = yourPies;
+export const Watchlist:React.FC = watchlist;
--- Challenge/app/screens/PieScreen.tsx
+++ Challenge/app/screens/PieScreen.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { ImageBackground, ScrollView, StyleSheet, View } from 'react-native';
 import { HeaderBackground } from '../assets/background';
+import { Header } from '../shared/components';
+import { Balance, Watchlist, YourPies } from '../components/pie';
 
 const PieScreen: React.FC = () => {
     return (
-        <View style={{ flex: 1 }}>
+        <ScrollView style={{ flex: 1 }}>
             <ImageBackground source={HeaderBackground} resizeMode="stretch" resizeMethod="resize" style={styles.headerBackground}>
-                <Text>@ethansmith</Text>
+                <View style={styles.container}>
+                    <Header />
+                    <Balance />
+                    <YourPies />
+                    <Watchlist />
+                </View>
             </ImageBackground>
-        </View>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
+    container: { flex: 1, paddingVertical: 32 },
     headerBackground: {
-      flex: 1,
-      width: "100%",
-      height: "40%",
-      borderBottomLeftRadius: 20,
-      borderBottomRightRadius: 20,
+        flex: 1,
+        width: "100%",
+        height: "45%",
+        borderBottomLeftRadius: 20,
+        borderBottomRightRadius: 20,
     },
-  });
+    header: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    nameTagContainer: {
+        alignSelf: "flex-start",
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        borderRadius: 50,
+    },
+    nameTag: {
+        color: "#fff",
+        fontWeight: "600",
+        fontSize: 16,
+    },
+    capIconContainer: {
+        alignSelf: 'flex-start',
+        backgroundColor: "#E9E7FE",
+        padding: 7,
+        borderRadius: 12,
+    }
+});
 
 export default PieScreen;
