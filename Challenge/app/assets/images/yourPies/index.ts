e167ae1c5f390ecbe2dad26600ae4eaae35c8f32
Valeria Biaggi <vbiaggi10@gmail.com>
2021-09-20 09:36:05 -0500

    Set icons and config libraries

---
+++ Challenge/app/assets/images/yourPies/index.ts
@@ -0,0 +1,7 @@
+import techChart from './tech-chart.png';
+import communicationChart from './communication-chart.png';
+import energyChart from './energy-chart.png';
+
+export const TechChart:string = techChart;
+export const CommunicationChart:string = communicationChart;
+export const EnergyChart:string = energyChart;
